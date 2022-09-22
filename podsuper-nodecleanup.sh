38064bef13f03ca8eefd2d8098b1d057d3444ed5
gwest <gwest@gwest@redhat.com-mac>
2022-09-22 10:00:46 -0500

    Add cleanup script for node

---
+++ podsuper-nodecleanup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+oc get pod --all-namespaces -o wide | tail -n +2  >  data/pods.data
+input="data/pods.data"
+while IFS= read -r line
+#Example data
+#testapp                                            httpd-ex-1-build                                         0/1     Completed   0          19h     10.128.2.107    worker-1          <none>           <none>
+do
+    #echo "$line"
+    namespace=`echo $line | cut -d' ' -f1`
+    podname=`echo $line | cut -d' ' -f2`
+    nodename=`echo $line | cut -d' ' -f8`
+    ocpstr='openshift'
+    if [[ "$namespace" != *$ocpstr* ]]; then
+         if [[ "$nodename" == $1 ]]; then
+            echo "Cleaning up $nodename/$namespace/$podname"
+            oc delete po/$podname
+           fi
+       fi
+done < "$input"
+
