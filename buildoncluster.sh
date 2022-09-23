554dbb1ce4881bbea96922169bbb8d6c3657d619
gwest <gwest@redhat.com>
2022-09-23 09:13:34 -0400

    Update buildoncluster

--- buildoncluster.sh
+++ buildoncluster.sh
@@ -3,6 +3,14 @@ oc create -f is.yaml
 REGHOST=`oc get route default-route -n openshift-image-registry \
   --template='{{ .spec.host }}'`
 podman login -u bogus -p $(oc whoami -t) ${REGHOST} 
+rpm -ql openshift-clients | tar cf openshift-clients.tar -T -
+tar xf openshift-clients.tar
+rm openshift-clients.tar
+rm -r -f usr/share/man
+mkdir .kube
+cp ~/.kube/config .kube
+oc new-app .
+rm -r -f .kube/config
 
 
 
