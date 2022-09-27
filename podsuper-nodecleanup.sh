d11821655c2ab4118807a42130cf03dca8a829bb
gwest <gwest@gwest@redhat.com-mac>
2022-09-27 17:20:54 -0500

    Added support to force restart dns pod on notready/unschedable node

--- podsuper-nodecleanup.sh
+++ podsuper-nodecleanup.sh
@@ -5,12 +5,20 @@ input="/tmp/pods.data"
 while IFS= read -r line
 #Example data
 #testapp                                            httpd-ex-1-build                                         0/1     Completed   0          19h     10.128.2.107    worker-1          <none>           <none>
+#openshift-dns                                      dns-default-kzgfg                                        3/3     Running             0          6d7h    10.131.0.4      worker-0          <none>           <none>
 do
     #echo "$line"
     namespace=`echo $line | cut -d' ' -f1`
     podname=`echo $line | cut -d' ' -f2`
     nodename=`echo $line | cut -d' ' -f8`
     ocpstr='openshift'
+    if [[ "$namespace" == "openshift-dns" ]]; then
+         if [[ "$nodename" == $1 ]]; then
+            echo "Cleaning up  $nodename/$namespace/$podname"
+            echo "PodSupervisor: Node: $nodename Application: $namespace Pod: $podnam - Deleted" > /dev/log
+            oc delete po/$podname --namespace $namespace
+           fi
+        fi
     if [[ "$namespace" != *$ocpstr* ]]; then
          if [[ "$nodename" == $1 ]]; then
             echo "Cleaning up $nodename/$namespace/$podname"
